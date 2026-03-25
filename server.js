983bcf55a9187b7c89a38feabb4662a390ed8829
Ariana Inamdar <ai106@wellesley.edu>
2026-03-25 15:55:38 -0400

    home page added to server

---
+++ server.js
@@ -0,0 +1,50 @@
+const path = require('path');
+require("dotenv").config({ path: path.join(process.env.HOME, '.cs304env')});
+const express = require('express');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+
+// our modules loaded from cwd
+
+const { Connection } = require('./connection');
+const cs304 = require('./cs304');
+const { add, result } = require('lodash');
+
+// Create and configure the app
+
+const app = express();
+
+// Morgan reports the final status code of a request's response
+app.use(morgan('tiny'));
+
+app.use(cs304.logStartRequest);
+
+// This handles POST data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cs304.logRequestData);  // tell the user about any request data
+
+app.use(serveStatic('public'));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const mongoUri = cs304.getMongoUri();
+
+
+//home page results in the home ejs file
+app.get('/', (req, res) => {
+    res.render('home.ejs');
+})
+
+
+//--------------------------- last --------------------------------
+
+const serverPort = cs304.getPort(8080);
+
+// this is last, because it never returns
+app.listen(serverPort, function() {
+    console.log(`open http://localhost:${serverPort}`);
+});
